99e1bc754f5bebe6e452e4ebe4adbbc0eaafb669
Sreenivasa Reddy Saddikuti <dlms.sadddikuti@gmail.com>
2021-01-02 13:08:47 +0000

    added files

---
+++ coreprac/src/main/java/com/poortoys/examples/AppMain.java
@@ -0,0 +1,11 @@
+package com.poortoys.examples;
+
+public class AppMain {
+
+    public static void main(String[] args) {
+    	
+    	int test=123;
+        System.out.println("Hello, world");
+    }
+
+}
